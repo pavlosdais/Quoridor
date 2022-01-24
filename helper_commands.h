c2f8f6c526d90cbf1479cb20b7bb23f10e7522c8
stavrosprentzas <97403252+stavrosprentzas@users.noreply.github.com>
2022-01-24 22:15:56 +0200

    Update helper_commands.h

--- helper_commands.h
+++ helper_commands.h
@@ -1,10 +1,10 @@
-typedef struct position position;
+typedef struct player player;
 
 char isnumber(char *n);
-void reset_pawns(int boardsize, position *white, position *black);
+void reset_pawns(int boardsize, player *white, player *black);
 void swap_boardsize(char* p, int *boardsize, int *prev_boardsize);
-char is_x_available(char hor, position white);
-char is_y_availabe(char hor, position black);
-position *check_color(char *p, position *black, position *white);
+char is_x_available(char hor, player white);
+char is_y_availabe(char hor, player black);
+player *check_color(char *p, player *black, position *white);
 char check_orientation(char *orientation);
 char arguments(char *buff);
