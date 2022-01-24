12cfd37ddc1675934e5db7df984d99c84a8fa3d7
stavrosprentzas <97403252+stavrosprentzas@users.noreply.github.com>
2022-01-24 22:16:52 +0200

    Update helper_commands.h

--- helper_commands.h
+++ helper_commands.h
@@ -5,6 +5,6 @@ void reset_pawns(int boardsize, player *white, player *black);
 void swap_boardsize(char* p, int *boardsize, int *prev_boardsize);
 char is_x_available(char hor, player white);
 char is_y_availabe(char hor, player black);
-player *check_color(char *p, player *black, position *white);
+player *check_color(char *p, player *black, player *white);
 char check_orientation(char *orientation);
 char arguments(char *buff);
