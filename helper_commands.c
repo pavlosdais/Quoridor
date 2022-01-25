b700b78d79dbd1ac7bd3e8633e0dcd830f95e6b1
stavrosprentzas <97403252+stavrosprentzas@users.noreply.github.com>
2022-01-25 03:07:13 +0200

    Update helper_commands.c

--- helper_commands.c
+++ helper_commands.c
@@ -93,23 +93,23 @@ char arguments(char *buff)
     return arg;
 }
 
-char wallBelow(int i, int j, char **w_mtx, int bd_sz) {
+char wallBelow(int i, int j, char **w_mtx, int boardsize) {
     if (i==0) return 0;
     return (w_mtx[i][j]=='b' || (j>0 && w_mtx[i][j-1]=='b'));
 }
 
-char wallAbove(int i, int j, char **w_mtx, int bd_sz) {
-    if (i==bd_sz-1) return 0;
-    return wallBelow(i+1, j, w_mtx, bd_sz);
+char wallAbove(int i, int j, char **w_mtx, int boardsize) {
+    if (i==boardsize-1) return 0;
+    return wallBelow(i+1, j, w_mtx, boardsize);
 }
 
-char wallOnTheRight(int i, int j, char **w_mtx, int bd_sz) {
+char wallOnTheRight(int i, int j, char **w_mtx, int boardsize) {
     if (j==bd_sz-1) return 0;
-    return (w_mtx[i][j]=='r' || (i<bd_sz-1 && w_mtx[i+1][j]=='r'));
+    return (w_mtx[i][j]=='r' || (i<boardsize-1 && w_mtx[i+1][j]=='r'));
 }
 
-char wallOnTheLeft(int i, int j, char **w_mtx, int bd_sz) {
+char wallOnTheLeft(int i, int j, char **w_mtx, int boardsize) {
     if (j==0) return 0;
-    return wallOnTheRight(i, j-1, w_mtx, bd_sz);
+    return wallOnTheRight(i, j-1, w_mtx, boardsize);
 }
 
