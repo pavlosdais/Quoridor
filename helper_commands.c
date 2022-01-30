f51e379b5fa2273941618e1cc4a68b54361af897
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-01-30 14:48:20 +0200

    Update helper_commands.c

--- helper_commands.c
+++ helper_commands.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
+#define ABS(a, b) (a-b > 0) ? a-b:b-a
+
 typedef struct player {
     int i;
     int j;
@@ -30,8 +32,9 @@ void reset_pawns(int boardsize, player *white, player *black)
 {
     white->i = 0;
     white->j = boardsize / 2;
-    black->j = boardsize / 2;
+
     black->i = boardsize - 1;
+    black->j = boardsize / 2;
 }
 
 void swap_boardsize(char* p, int *boardsize, int *prev_boardsize)
@@ -46,22 +49,19 @@ void swap_boardsize(char* p, int *boardsize, int *prev_boardsize)
     *boardsize = atoi(p);
 }
 
-char is_x_available(char hor, player white)
+char is_x_available(char hor, int boardsize)
 {
-    if (hor >= 'A' && hor < (white.j + 'A'))
-    {
-        return 1;
-    }
-    else if (hor >= 'a' && hor < (white.j + 'a'))
+    if (hor >= 0 && hor < boardsize)
     {
         return 1;
     }
     return 0;
+
 }
 
-char is_y_availabe(char hor, player black)
+char is_y_availabe(char hor, int boardsize)
 {
-    if (hor >= 0 && hor < black.i)
+    if (hor >= 0 && hor < boardsize)
     {
         return 1;
     }
@@ -70,7 +70,7 @@ char is_y_availabe(char hor, player black)
 
 player *check_color(char *p, player *black, player *white)
 {
-    // function returns 1 for white, 0 for black and -1 for unknown color
+    // function returns the player with the corresponding color 
 
     if (strcmp(p, "white") == 0) return white;
     else if (strcmp(p, "w") == 0) return white;
@@ -107,6 +107,15 @@ char wallOnTheLeft(int i, int j, char **w_mtx, int boardsize) {
     return wallOnTheRight(i, j-1, w_mtx, boardsize);
 }
 
+char there_is_a_wall(int i, int j, char **wall_matrix, int boardsize)
+{
+    if (wallOnTheLeft(i, j, wall_matrix, boardsize) ) return 1;
+    else if (wallOnTheLeft(i, j, wall_matrix, boardsize) ) return 1;
+    else if (wallAbove(i, j, wall_matrix, boardsize) ) return 1;
+    else if (wallBelow(i, j, wall_matrix, boardsize) ) return 1;
+    else return 0;
+}
+
 char enough_arguments(char *argument)
 {
     if (argument == NULL)   // not a valid argument 
@@ -117,4 +126,140 @@ char enough_arguments(char *argument)
     }
     // in any other case, a valid argument
     return 1;
+}
+char recursiveSolveBlack(int x, int y, char **have_visited, char **maze, char **correct_path, int size);
+char recursiveSolveWhite(int x, int y, char **have_visited, char **maze, char **correct_path, int size);
+int steps(char **correct_path, int x, int y);
+void free_grids(char **A, char **B, int size);
+
+int path_steps(char **wall_matrix, int boardsize, player *pl, char color)
+{
+    int i, j;
+    char **have_visited = malloc(boardsize*sizeof(boardsize));
+    char **correct_path = malloc(boardsize*sizeof(boardsize));
+
+    for (i = 0; i < boardsize; i++)
+    {
+        have_visited[i] = malloc(sizeof(char) * boardsize);
+        correct_path[i] = malloc(sizeof(char) * boardsize);
+    }
+
+    char f;
+    if (color == 1) // white
+    {
+        f = recursiveSolveWhite(pl->i, pl->j, have_visited, wall_matrix, correct_path, boardsize);
+    }
+    else // black
+    {
+        f = recursiveSolveBlack(pl->i, pl->j, have_visited, wall_matrix, correct_path, boardsize);
+    }
+    if (f)
+    {   
+        return steps(correct_path, boardsize-1, boardsize-1);
+    }
+    return 0;
+    free_grids(have_visited, correct_path, boardsize);
+}
+
+char recursiveSolveBlack(int x, int y, char **have_visited, char **maze, char **correct_path, int size)
+{
+    // black wins when the pawn gets to the first row
+
+    if (x == 0) return 1;  // black has reached the end
+    if (there_is_a_wall(x, y, maze, size) || have_visited[x][y]) return 0;
+
+    have_visited[x][y] = 1;
+
+    // search up
+    if (y != 0)
+    {
+        if (recursiveSolveBlack(x, y-1, have_visited, maze, correct_path, size))
+        {
+            correct_path[x][y] = 1;
+            return 1;
+        }
+    }
+    // search left
+    if (recursiveSolveBlack(x-1, y, have_visited, maze, correct_path, size))
+    {
+        correct_path[x][y] = 1;
+        return 1;
+    }
+    // search right
+    if (x != size - 1)
+    {
+        if (recursiveSolveBlack(x+1, y, have_visited, maze, correct_path, size))
+        {
+            correct_path[x][y] = 1;
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+char recursiveSolveWhite(int x, int y, char **have_visited, char **maze, char **correct_path, int size)
+{
+    // white wins when the pawn gets to the last row
+
+    if (x == size - 1) return 1;  // white has reached the end
+    if (there_is_a_wall(x, y, maze, size) || have_visited[x][y]) return 0;
+
+    have_visited[x][y] = 1;
+
+    // search down
+    if (y != size - 1)
+    {
+        if (recursiveSolveWhite(x, y+1, have_visited, maze, correct_path, size))
+        {
+            correct_path[x][y] = 1;
+            return 1;
+        }
+    }
+    // search left
+    if (x != 0)
+    {
+        if (recursiveSolveWhite(x-1, y, have_visited, maze, correct_path, size))
+        {
+            correct_path[x][y] = 1;
+            return 1;
+        }
+    }
+    // search right
+    if (recursiveSolveWhite(x+1, y, have_visited, maze, correct_path, size))
+    {
+        correct_path[x][y] = 1;
+        return 1;
+    }
+
+    return 0;
+}
+
+int steps(char **correct_path, int x, int y)
+{
+    int total = 0;
+    for (int i = 0; i < x; i ++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            total += correct_path[i][j];
+        }
+    }
+    return total;
+}
+
+char is_legal_wall(char **wall_matrix, int boardsize, player *cur_player, player *opponent, int x, int y)
+{
+    if (cur_player->walls == 0) return 0;  // no available walls to be placed
+}
+
+void free_grids(char **A, char **B, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        free(A[i]);
+        free(B[i]);
+    }
+    free(A);
+    free(B);
 }
