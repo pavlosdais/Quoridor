b3c4bcdba45f0bc92e0dd3dd10af78e65426718e
stavrosprentzas <97403252+stavrosprentzas@users.noreply.github.com>
2022-02-15 03:36:52 +0200

    Update commands.h

--- commands.h
+++ commands.h
@@ -1,11 +1,19 @@
 typedef struct player player;
 
-char isnumber(char *n);
+void unsuccessful_response(char *msg);
+void successful_response(char *msg);
+char **allocate_memory(int boardsize);
+void free_array(char **A, int boardsize);
+char command_num(char *ans);
+void print_name(char *p);
+void known_command(void);
+void list_commands();
+void update_boardsize(int *boardsize, int *prev_boardsize, char ***wall_matrix, player *white, player *black);
+void clear_board(int boardsize, char **wall_matrix, player *white, player *black);
+void update_walls(player *black, player *white, int* number_of_walls);
+void playmove(char *buff, player *white, player *black, char** wall_matrix, int boardsize);
+void playwall(char *buff, player *white, player *black, char** wall_matrix, int boardsize);
+void winner(player *white, player *black, int boardsize);
+void showboard(char **w_mtx, int boardsize, player *black, player *white);
+void command_preprocess(char *buff);
 void reset_pawns(int boardsize, player *white, player *black);
-void swap_boardsize(char* p, int *boardsize, int *prev_boardsize);
-char is_vertex_available(char hor, int boardsize);
-player *check_color(char *p, player *black, player *white);
-char check_orientation(char *orientation);
-char there_is_a_wall(int i, int j, char **wall_matrix, int boardsize);
-char enough_arguments(char *argument);
-char there_is_a_path(char **wall_matrix, int boardsize, player *white, player *black);
