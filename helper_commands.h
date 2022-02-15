f93f6991be265d5cf1edd8474e4afb6170d965aa
stavrosprentzas <97403252+stavrosprentzas@users.noreply.github.com>
2022-02-15 21:49:46 +0200

    Update helper_commands.h

--- helper_commands.h
+++ helper_commands.h
@@ -16,4 +16,4 @@ char enough_arguments(char *argument);
 int path_steps(char **wall_matrix, int boardsize, player *pl, char color);
 char there_is_a_wall(int i, int j, char **wall_matrix, int boardsize);
 char there_is_a_path(char **wall_matrix, int boardsize, player *white, player *black);
-void addMove(stackptr *last, int i, int j, int type);
+void addMove(stackptr *last, int i, int j, char type);
