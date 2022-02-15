2ea4faa9895604e095e2b7e333f0378c8d40b94c
stavrosprentzas <97403252+stavrosprentzas@users.noreply.github.com>
2022-02-15 03:36:23 +0200

    Update helper_commands.h

--- helper_commands.h
+++ helper_commands.h
@@ -1,10 +1,10 @@
 typedef struct player player;
 
 char isnumber(char *n);
-void reset_pawns(int boardsize, player *white, player *black);
 void swap_boardsize(char* p, int *boardsize, int *prev_boardsize);
 char is_x_available(char hor, int boardsize);
 char is_y_availabe(char hor, int boardsize);
+char is_vertex_available(char hor, int boardsize)
 player *check_color(char *p, player *black, player *white);
 char check_orientation(char *orientation);
 char wallBelow(int i, int j, char **w_mtx, int boardsize);
@@ -13,3 +13,5 @@ char wallOnTheRight(int i, int j, char **w_mtx, int boardsize);
 char wallOnTheLeft(int i, int j, char **w_mtx, int boardsize);
 char enough_arguments(char *argument);
 int path_steps(char **wall_matrix, int boardsize, player *pl, char color);
+char there_is_a_wall(int i, int j, char **wall_matrix, int boardsize);
+char there_is_a_path(char **wall_matrix, int boardsize, player *white, player *black);
