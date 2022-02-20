e51809f1e86b8d6d46d556f2ee794781b0a1acc5
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-02-20 21:20:01 +0200

    Update helper_commands.c

--- helper_commands.c
+++ helper_commands.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <float.h>
 #include "bfs.h"
 
 typedef struct player {
@@ -17,19 +18,17 @@ struct stacknode {
     stackptr next;
 };
 
-char isnumber(char *n)
+char isNumber(char *n)
 {
     int i = 0;
     if (n[0] == '-') i = 1;
     while(n[i] != '\n' && n[i] != '\0')
     {
         if (!isdigit(n[i]))
-        {
-            return 0;
-        }
+            return 0;  // not a number
         i++;
     }
-    if (n[0] == '-' && i == 1) return 0;
+    if (n[0] == '-' && i == 1) return 0;  // not a number
     return 1;
 }
 
@@ -48,30 +47,37 @@ void swap_boardsize(char* p, int *boardsize, int *prev_boardsize)
 char is_vertex_valid(char hor, int boardsize)
 {
     if (hor >= 0 && hor < boardsize)
-    {
         return 1;
-    }
     return 0;
 }
 
 player *check_color(char *p, player *black, player *white)
 {
     // function returns the player with the corresponding color
-    if (!strcmp(p, "white")) return white;
-    else if (!strcmp(p, "w")) return white;
-    else if (!strcmp(p, "black")) return black;
-    else if (!strcmp(p, "b")) return white;
-    else return NULL;
+    if (!strcmp(p, "white")) 
+        return white;
+    else if (!strcmp(p, "w"))
+        return white;
+    else if (!strcmp(p, "black"))
+        return black;
+    else if (!strcmp(p, "b"))
+        return white;
+    else  // error
+        return NULL;
 }
 
 char check_orientation(char *orientation)
 {
     // function returns b for horizontal, r for vertical and -1 for unknown orientation
-    if (!strcmp(orientation, "horizontal")) return 'b';
-    else if (!strcmp(orientation, "h")) return 'b';
-    else if (!strcmp(orientation, "vertical")) return 'r';
-    else if (!strcmp(orientation, "v")) return 'r';
-    return -1;
+    if (!strcmp(orientation, "horizontal"))
+        return 'b';
+    else if (!strcmp(orientation, "h"))
+        return 'b';
+    else if (!strcmp(orientation, "vertical"))
+        return 'r';
+    else if (!strcmp(orientation, "v"))
+        return 'r'; 
+    return -1; // error
 }
 
 char enough_arguments(char *argument)
@@ -89,12 +95,14 @@ char enough_arguments(char *argument)
 char there_is_a_path(char **wall_matrix, int boardsize, player *white, player *black)
 {
     int steps = bfs(boardsize, wall_matrix, black->i, black->j, 0);  // black wins if he gets to the first row
-    if (steps == -1) return 0;
+    if (steps == -1)
+        return 0;  // path is blocked for black
 
     steps = bfs(boardsize, wall_matrix, white->i, white->j, boardsize-1);  // white wins if he gets to the last row
-    if (steps == -1) return 0;
+    if (steps == -1)
+        return 0;  // path is blocked for white
 
-    return 1;
+    return 1;  // the path is not being blocked
 }
 
 void addMove(stackptr *last, int i, int j, char *type)
@@ -113,4 +121,27 @@ float positionEvaluation(player* black, player* white, int boardsize, char** wal
     int blackDistance = bfs(boardsize, wall_matrix, black->i, black->j, 0);
    
     return whiteDistance-blackDistance + 0.2*(white->walls - black->walls);
+}
+
+float max(float a, float b)
+{
+    if (a >= b) return a;
+    return b;
+}
+
+float minimax(char**wall_matrix, int boardsize, char depth, float alpha, float beta, char maximizingPlayer, player* black, player* white)
+{
+    if (depth == 0 || white->i==boardsize-1 || black->i==0)  // End of search
+    {
+        return positionEvaluation(black, white, boardsize, wall_matrix);
+    }
+
+    if (maximizingPlayer)
+    {
+        float max_eval = FLT_MIN;
+    }
+    else  // !maximizingPlayer
+    {
+        float min_eval = FLT_MAX;
+    }
 }
