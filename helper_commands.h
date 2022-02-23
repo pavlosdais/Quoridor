bdd7b001546fe1ab6d501f36f790be87e19705a9
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-02-24 00:29:36 +0200

    Update helper_commands.h

--- helper_commands.h
+++ helper_commands.h
@@ -17,3 +17,4 @@ char wallAbove(int i, int j, char **w_mtx, int boardsize);
 char wallOnTheRight(int i, int j, char **w_mtx, int boardsize);
 char wallOnTheLeft(int i, int j, char **w_mtx, int boardsize);
 char there_is_a_wall(int i, int j, char **wall_matrix, int boardsize);
+float max(float a, float b);
