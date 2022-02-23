66fdc1de7734349b336787b49a4e24b3744b84d5
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-02-24 00:28:02 +0200

    Update ai.c

--- ai.c
+++ ai.c
@@ -10,12 +10,7 @@ typedef struct player {
     int walls;
 } player;
 
-typedef struct stacknode *stackptr;
-struct stacknode {
-    int i,j;
-    char *type; // 'b': black player left (i,j), 'w': white player left (i,j), 'n': new wall placed at (i,j)
-    stackptr next;
-};
+// initial call -> minimax(currentPosition, depth, -infinity, +infinity, true)
 
 float minimax(char**wall_matrix, int boardsize, char depth, float alpha, float beta, char maximizingPlayer, player* black, player* white)
 {
@@ -24,12 +19,136 @@ float minimax(char**wall_matrix, int boardsize, char depth, float alpha, float b
         return positionEvaluation(black, white, boardsize, wall_matrix);
     }
 
-    if (maximizingPlayer)
+    int eval;
+    if (maximizingPlayer)  // white
     {
         float max_eval = FLT_MIN;
+
+        // check each possible move
+
+        // check pawn advancement
+        
+        if (!wallBelow(white->i, white->j, wall_matrix, boardsize)) // up
+        {
+            --white->i;  
+            eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+            alpha = max(alpha, eval);
+            ++white->i;  // reset
+        }
+
+        if (!wallOnTheRight(white->i, white->j, wall_matrix, boardsize))  // right
+        {
+            ++white->j;
+            eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+            alpha = max(alpha, eval);
+            --white->j;  // reset
+        }
+
+        if (!wallOnTheLeft(white->i, white->j, wall_matrix, boardsize))  // left
+        {
+            --white->j;
+            eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+            alpha = max(alpha, eval);
+            ++white->j;  // reset
+        }
+        
+        if (wallOnTheLeft(white->i, white->j, wall_matrix, boardsize) && wallOnTheRight(white->i, white->j, wall_matrix, boardsize) && wallBelow(white->i, white->j, wall_matrix, boardsize))  // boxed in - go down
+        {
+            if (!wallAbove(white->i, white->j, wall_matrix, boardsize))
+            {
+                ++white ->i;
+                eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+                alpha = max(alpha, eval);
+                --white->i;  // reset
+            }
+        }
+
+        // check wall placement
+        for (int i = 1; i < boardsize; i++)
+        {
+            for (int j = 1; j < boardsize; j++)
+            {
+                if (!there_is_a_wall(i, j, wall_matrix, boardsize))
+                {
+                    wall_matrix[i][j] = 'b';  // place wall below
+                    if (!there_is_a_path(wall_matrix, boardsize, white, black)) wall_matrix[i][j] = 0;
+                    else eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+
+                    wall_matrix[i][j] = 'r';  // place wall on the right
+                    if (!there_is_a_path(wall_matrix, boardsize, white, black)) wall_matrix[i][j] = 0;
+                    else eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+
+                    wall_matrix[i][j] = 0;  // reset placement
+                    alpha = max(alpha, eval);
+                    if (beta <= alpha) break;
+                }
+            }
+        }
+        return max_eval;
     }
-    else  // !maximizingPlayer
+    else  // !maximizingPlayer - black
     {
         float min_eval = FLT_MAX;
+        // check each possible move
+        
+        // check pawn advancement
+
+        if (!wallAbove(black->i, black->j, wall_matrix, boardsize))  // down
+        {
+            ++black->i;  
+            eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+            alpha = max(alpha, eval);
+            --black->i;  // reset
+        }
+        
+        if (!wallOnTheRight(black->i, black->j, wall_matrix, boardsize))  // right
+        {
+            ++black->j;
+            eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+            alpha = max(alpha, eval);
+            --black->j;  // reset
+        }
+
+        if (!wallOnTheLeft(black->i, black->j, wall_matrix, boardsize))  // left
+        {
+            --black->j;
+            eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+            alpha = max(alpha, eval);
+            ++black->j;  // reset
+        }
+        
+        if (wallOnTheLeft(black->i, black->j, wall_matrix, boardsize) && wallOnTheRight(white->i, white->j, wall_matrix, boardsize) && wallAbove(white->i, white->j, wall_matrix, boardsize))  // boxed in - go up
+        {
+            if (!wallBelow(black->i, black->j, wall_matrix, boardsize))
+            {
+                --black->i;
+                eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 0, black, white);
+                alpha = max(alpha, eval);
+                ++black->i;  // reset
+            }
+        }
+
+        // check wall placement
+        for (int i = 1; i <= boardsize-1; i++)
+        {
+            for (int j = 1; j <= boardsize-1; j++)
+            {
+                if (!there_is_a_wall(i, j, wall_matrix, boardsize))
+                {
+                    wall_matrix[i][j] = 'b';  // place wall below
+                    if (!there_is_a_path(wall_matrix, boardsize, white, black)) wall_matrix[i][j] = 0;
+                    else eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 1, black, white);
+
+                    wall_matrix[i][j] = 'r';  // place wall on the right
+                    if (!there_is_a_path(wall_matrix, boardsize, white, black)) wall_matrix[i][j] = 0;
+                    eval = minimax(wall_matrix, boardsize, depth-1, alpha, beta, 1, black, white);
+
+                    wall_matrix[i][j] = 0;  // reset placement
+                    alpha = max(alpha, eval);
+                    if (beta <= alpha) break;
+                }
+            }
+        }
+        return min_eval;
     }
 }
