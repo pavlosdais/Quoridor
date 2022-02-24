6f2a6f502958f44ae793c1a5dee892a84f51c2ec
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-02-25 01:55:22 +0200

    Delete ai.h

--- ai.h
+++
@@ -1,2 +0,0 @@
-typedef struct player player;
-float minimax(char**wall_matrix, int boardsize, char depth, float alpha, float beta, char maximizingPlayer, player* black, player* white, char pl, char *move, int *x, int *y, char* or);
