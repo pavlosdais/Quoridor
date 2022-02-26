ae30457a067e2bbf737e097efdadaab30c02c516
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-02-26 03:44:34 +0200

    Delete ai.h

--- ai.h
+++
@@ -1,4 +0,0 @@
-typedef struct player player;
-typedef struct returningMove returningMove;
-returningMove *bestMove(char** w_mtx, int boardsize, char pl, player* black, player* white, char depth);
-float minimax(char** wall_matrix, int boardsize, char depth, float alpha, float beta, player* maximizing, player* minimizing, char pl, char maximizingPlays, player *black, player *white);
