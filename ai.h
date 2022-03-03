ac9e19c84f64c2afcf025f01ab05bd08e8396358
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-03-03 18:18:47 +0200

    Update ai.h

--- ai.h
+++ ai.h
@@ -1,3 +1,3 @@
 typedef struct player player;
 typedef struct returningMove returningMove;
-returningMove bestMove(char** wall_matrix, int boardsize, char pl, player* black, player* white, unsigned char depth);
+returningMove bestMove(char** wall_matrix, int boardsize, char pl, player* black, player* white, unsigned char depth, char pseudo);
