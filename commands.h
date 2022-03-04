39d87f86eef1d3f5aa327673e04128f9ba5f9bb4
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-03-04 15:39:09 +0200

    Update commands.h

--- commands.h
+++ commands.h
@@ -8,8 +8,8 @@ char update_boardsize(int* boardsize, int* prev_boardsize, char*** wall_matrix, 
 void clear_board(int boardsize, char** wall_matrix, player white, player black, stackptr* history, int* totalmoves);
 void update_walls(player* white, player* black, int* number_of_walls);
 void playmove(char* buff, player* white, player* black, char** wall_matrix, int boardsize, stackptr* history, int* totalmoves);
-void playwall(char* buff, player* white, player* black, char** wall_matrix, int boardsize, stackptr* history, int* totalmoves);
-void genmove(player* white, player* black, char** wall_matrix, int boardsize, stackptr* lastaddr, int* totalmoves);
+char playwall(char* buff, player* white, player* black, char** wall_matrix, int boardsize, stackptr* history, int* totalmoves);
+char genmove(player* white, player* black, char** wall_matrix, int boardsize, stackptr* lastaddr, int* totalmoves);
 void undo(char** wall_matrix, player* white, player* black, stackptr* last, int* totalmoves);
 void winner(player white, player black, int boardsize);
 void showboard(char** wall_matrix, int boardsize, player* white, player* black);
