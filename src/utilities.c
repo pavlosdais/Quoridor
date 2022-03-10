5c31347a3af0a50d3f0a121b60398845a6deb581
Pavlos Dais <60575068+pavlosdais@users.noreply.github.com>
2022-03-10 03:17:46 +0200

    Update utilities.c

--- src/utilities.c
+++ src/utilities.c
@@ -242,14 +242,9 @@ unsigned char findDepth(int boardsize, char* pseudo)
         *pseudo = 0;
         return 2;
     }
-    else if (boardsize <= 23)  // pseudodepth works at 2
+    else  // pseudodepth works at 2
     {
         *pseudo = 1;
         return 2;
-    }
-    else  // depth 1
-    {
-        *pseudo = 0;
-        return 1;
     }
 }
